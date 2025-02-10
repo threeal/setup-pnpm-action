ca51668bdb4ed68bdc1493fe79a3a96c9115450e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-10 15:32:22 +0700

    feat: specify default pnpm version in action schema (#27)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- dist/main.bundle.mjs
+++ dist/main.bundle.mjs
@@ -131,9 +131,7 @@ async function setupPnpm(pnpmHome) {
 }
 
 try {
-    let version = getInput("version");
-    if (version === "")
-        version = "10.2.1";
+    const version = getInput("version");
     const platform = getPlatform();
     const architecture = getArchitecture();
     const pnpmHome = await createPnpmHome(version);
